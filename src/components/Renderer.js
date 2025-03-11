5e92f23941e7168c311a4790a5404a3066e94b3d
Gerard <gerardjalon@gmail.com>
2025-03-11 22:27:27 +0100

    Crear renderer

---
+++ src/components/Renderer.js
@@ -0,0 +1,17 @@
+import * as THREE from "three";
+
+export function Renderer() {
+    const canvas = document.querySelector("canvas.game");
+    if (!canvas) {
+        throw new Error("No canvas found");
+    };
+    const renderer = new THREE.WebGLRenderer({ 
+        alpha: true,
+        antialias: true,
+        canvas: canvas, 
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    
+    return renderer;
+}
